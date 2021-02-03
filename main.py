cddf036f83dfaea8aeee3e3314d34c485cf76eb7
Alex <aeversme@gmail.com>
2021-02-02 23:00:29 -0500

    add comments to define code sections

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import guizero as gui
 
+# App -----
 app = gui.App(title='Pixela Habit Tracker')
 app.bg = '#FFFAFA'
+
+# Functions -----
+
+# Widgets -----
 pixela_logo = gui.Picture(app, image='images/PIXELA_RED.png', width=60, height=60)
 welcome_message = gui.Text(app, text='Habit Tracker')
 welcome_message.text_size = 40
+
+# Display -----
 app.display()
