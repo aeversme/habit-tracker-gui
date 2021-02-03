6da222cb2d10c38088ba411cf322147b739e1e2a
Alex <aeversme@gmail.com>
2021-02-02 22:58:18 -0500

    resize icon and text

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import guizero as gui
 
 app = gui.App(title='Pixela Habit Tracker')
-message = gui.Text(app, text='Welcome to the Habit Tracker App')
+app.bg = '#FFFAFA'
+pixela_logo = gui.Picture(app, image='images/PIXELA_RED.png', width=60, height=60)
+welcome_message = gui.Text(app, text='Habit Tracker')
+welcome_message.text_size = 40
 app.display()
