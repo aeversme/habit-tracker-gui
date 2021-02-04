7d1cb92697a5e462052dd2ba99cedb67c52d02cd
Alex <aeversme@gmail.com>
2021-02-04 09:57:00 -0500

    add images and more widgets to app window

--- main.py
+++ main.py
@@ -8,8 +8,17 @@ app.bg = '#FFFAFA'
 
 
 def do_something():
-    changing_text.value = text_box.value
-    pixel_choice.value = pixel_action.value
+    changing_text.value = graph_name_text_box.value
+    pixel_choice.value = pixel_action.value_text
+    success_error_image.image = 'images/success.png'
+
+
+def reset():
+    quantity_text_box.clear()
+    graph_name_text_box.value = 'graph1'
+    dd.value = 'dd'
+    mm.value = 'mm'
+    yyyy.value = 'yyyy'
 
 
 # Widgets -----
@@ -17,19 +26,42 @@ pixela_logo = gui.Picture(app, image='images/PIXELA_RED.png', width=60, height=6
 banner_text = gui.Text(app, text='Habit Tracker')
 banner_text.text_size = 40
 
-instructions = gui.Text(app, text='Type something in the box and then push the button.')
+graph_name_text = gui.Text(app, text='Graph name:')
 
-text_box = gui.TextBox(app, '', width=20)
+graph_name_text_box = gui.TextBox(app, 'graph1', width=10)
 
-button = gui.PushButton(app, do_something, text='PUSH ME')
-button.bg = 'red'
-button.text_color = 'white'
+submit_button = gui.PushButton(app, do_something, text='Submit')
+reset_button = gui.PushButton(app, reset, text='Reset')
 
 changing_text = gui.Text(app, text='')
 
-pixel_action = gui.ButtonGroup(app, options=['Add a pixel', 'Modify a pixel', 'Delete a pixel'], selected='Add a pixel')
+pixel_action = gui.ButtonGroup(app,
+                               options=[
+                                   ['Add a pixel', 'add'],
+                                   ['Modify a pixel', 'modify'],
+                                   ['Delete a pixel', 'delete']
+                               ],
+                               horizontal='False',
+                               selected='add')
 
 pixel_choice = gui.Text(app, text='')
+
+day_selection = gui.ButtonGroup(app,
+                                options=[
+                                    ['Today', 'today'],
+                                    ['Another day', 'other day']
+                                ],
+                                horizontal='False',
+                                selected='today')
+
+dd = gui.TextBox(app, text='dd', width=4)
+mm = gui.TextBox(app, text='mm', width=4)
+yyyy = gui.TextBox(app, text='yyyy', width=4)
+
+quantity_text = gui.Text(app, text='Quantity: ')
+quantity_text_box = gui.TextBox(app, text='', width=4)
+
+success_error_image = gui.Picture(app, image='images/blank.png')
 
 # Display -----
 app.display()
