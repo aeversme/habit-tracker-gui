93eb8707b2323e732f8cd1f47734eac4cc66f7e7
Alex <aeversme@gmail.com>
2021-02-06 14:20:56 -0500

    add apioperations with stub functions

---
+++ apioperations.py
@@ -0,0 +1,16 @@
+import requests
+import secrets
+
+PIXELA_ENDPOINT = 'https://pixe.la/v1/users'
+
+
+def post_new_pixel():
+    pass
+
+
+def put_pixel_modification():
+    pass
+
+
+def delete_existing_pixel():
+    pass
